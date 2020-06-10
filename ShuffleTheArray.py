ad396ab8698fa7c87562e8ff85587ece4052d337
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-06-10 10:32:07 -0500

    Create ShuffleTheArray.py

---
+++ ShuffleTheArray.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def shuffle(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: List[int]
+        """
+        res=[]
+        for i in range(len(nums)):
+            if i+n >len(nums)-1:
+                break
+            res.append(nums[i])
+            res.append(nums[i+n])
+        return res    
+            
+        
