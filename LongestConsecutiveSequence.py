0ec428518eb0f828ab6a2fd777ec62cd18eac870
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-06-10 20:19:07 -0500

    Create LongestConsecutiveSequence.py

---
+++ LongestConsecutiveSequence.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        else:
+            
+            nums=sorted(set(nums))
+            #print(nums.sort())
+            length=0
+            result=0
+        
+        
+            for i in range(len(nums)-1):
+                #print(nums[i])
+                if nums[i+1]==1+nums[i]:
+                    length+=1
+                    result=max(result,length)
+                else:
+                    
+                    print(result,length)
+                    length=0
+            return result+1
