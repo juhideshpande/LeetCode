ce88abe6bdedf79d5dbdaed946cf2a0d9a421595
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-06-10 19:49:36 -0500

    Create FirstMissingPositive.py

---
+++ FirstMissingPositive.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if(len(nums)==0):
+            return 1
+        else:
+            length=range(1,len(nums)+2)
+            for i in range(len(length)):
+                if length[i] not in nums:
+                    return length[i]
+                
