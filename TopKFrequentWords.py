05abed1c8183a9f520d772d8c551a3a0a714b827
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-06-12 10:44:13 -0500

    Create TopKFrequentWords.py

---
+++ TopKFrequentWords.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def topKFrequent(self, words, k):
+        """
+        :type words: List[str]
+        :type k: int
+        :rtype: List[str]
+        """
+        
+        frequency={}
+       
+       
+        #count=0
+        for i in words:
+          #  print(words[i])
+            if i not in frequency:
+                frequency[i]=1
+            else:
+                frequency[i]+=1
+                
+        s1 = sorted(frequency, key=lambda w :(-frequency[w],w))  
+        return s1[:k]  
