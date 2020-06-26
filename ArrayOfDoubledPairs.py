056698a55bef6a43f3ae1e780a2820374700b775
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-06-25 23:51:34 -0500

    Update ArrayOfDoubledPairs.py

--- ArrayOfDoubledPairs.py
+++ ArrayOfDoubledPairs.py
@@ -4,11 +4,24 @@ class Solution(object):
         :type A: List[int]
         :rtype: bool
         """
-        pairs=Counter(A)
+#         pairs=Counter(A)
         
-        for i in sorted(pairs, key=abs):
-            if pairs[i]>pairs[2*i]:
+#         for i in sorted(pairs, key=abs):
+#             if pairs[i]>pairs[2*i]:
+#                 return False
+#             else:
+#                 pairs[2*i]-=pairs[i]
+#         return True    
+
+
+        count = collections.Counter(A)
+        for x in sorted(A, key = abs):
+            if count[x] == 0: 
+                continue
+            if count[2*x] == 0: 
                 return False
-            else:
-                pairs[2*i]-=pairs[i]
-        return True        
+            count[x] -= 1
+            count[2*x] -= 1
+            print(count)
+
+        return True
