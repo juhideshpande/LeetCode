7c5ea26b869eb3efcfbabe41de835eabd6389415
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-06-27 00:03:13 -0500

    Leetcode 441

---
+++ ArrangingCoins.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def arrangeCoins(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        i=1
+        
+        while n>=0:
+            n-=i
+            i+=1
+        return i-2    
+            
+                
