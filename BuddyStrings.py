31aaf505da8bbe2b3fc9d6dc7c1ba5b7c2f653ab
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-06-30 20:14:54 -0500

    Leetcode 859

---
+++ BuddyStrings.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def buddyStrings(self, A, B):
+        """
+        :type A: str
+        :type B: str
+        :rtype: bool
+        """
+        diff=[]
+        if len(A)!=len(B):
+            return False
+        
+        if A==B and len(set(A))< len(A):
+            return True
+        
+        for i in range(len(A)):
+            if A[i]!=B[i]:
+                diff.append((A[i],B[i]))
+                
+        return len(diff)==2 and diff[0]==diff[1][::-1]        
+            
+        
