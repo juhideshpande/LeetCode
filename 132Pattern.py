4e9d2bbb2d43ed69af0a70b77e7bbac95ba46184
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-07-02 21:04:54 -0500

    Leetcode 456

---
+++ 132Pattern.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def find132pattern(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        ans=[]
+        third=-sys.maxint
+        
+        for i in range(len(nums)-1,-1,-1):
+            if nums[i] < third:
+                return True
+            
+            while ans and ans[-1]<nums[i]:
+                third=ans.pop()
+                
+            ans.append(nums[i])        
+           
+        return False     
