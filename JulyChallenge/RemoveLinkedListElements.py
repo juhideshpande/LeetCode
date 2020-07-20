f1c39a7bf484853b953450459c2c5312083505c5
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-07-20 10:35:29 -0500

    LeetCode 203

---
+++ JulyChallenge/RemoveLinkedListElements.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        while head!=None and head.val==val:
+            head=head.next
+            
+        curr=ListNode(0)
+        curr=head
+        
+        while curr!=None and curr.next!=None:
+            if curr.next.val==val:
+                curr.next=curr.next.next
+                
+            else:
+                curr=curr.next
+                
+        return head       
