31e2aa0e92045d82d70fd4440c574683ff7bfff0
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-07-21 00:22:37 -0500

    LeetCode 398

---
+++ RandomPickIndex.py
@@ -0,0 +1,25 @@
+class Solution(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.array=nums
+
+    def pick(self, target):
+        """
+        :type target: int
+        :rtype: int
+        """
+        res = None
+        count = 0
+        for i, x in enumerate(self.array):
+            if x == target:
+                count += 1
+                chance = random.randint(1, count)
+                if chance == count:
+                    res = i
+        return res
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.pick(target)
