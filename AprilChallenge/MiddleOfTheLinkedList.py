2110526aa13a0a1ed0d59dea6c676a76d0a540c9
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-07-25 11:42:16 -0500

    LeetCode 876

---
+++ AprilChallenge/MiddleOfTheLinkedList.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        tmp = head
+        while tmp and tmp.next:
+            head = head.next
+            tmp = tmp.next.next
+        return head
+        
