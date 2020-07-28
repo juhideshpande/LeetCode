e3b38338e8bca51fa5f28be2f39d9a735c4f5c63
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-07-28 18:38:41 -0500

    LeetCode 402

---
+++ MayChallenge/RemoveKDigits.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def removeKdigits(self, num, k):
+        """
+        :type num: str
+        :type k: int
+        :rtype: str
+        """
+        stack = []
+        for ch in num:
+            while k > 0 and len(stack) > 0 and int(stack[-1]) > int(ch):
+                stack.pop()
+                k -= 1
+            print(stack)    
+            stack.append(ch)
+            print("--",stack,k)
+        
+        while k > 0:
+            stack.pop()
+            k -= 1
+        # print(stack)
+        return "0" if len(stack) ==0 else str(int("".join(stack)))
