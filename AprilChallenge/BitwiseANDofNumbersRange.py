24c83978a4dfcd03370507f8c4b81dcdd91f8622
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-08-02 11:03:43 -0500

    LeetCode 201

---
+++ AprilChallenge/BitwiseANDofNumbersRange.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def rangeBitwiseAnd(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        while(m!=n):            
+                m>>=1
+                n>>=1
+                count+=1
+            
+        while(count>0):
+            m<<=1
+            count-=1
+        return m    
