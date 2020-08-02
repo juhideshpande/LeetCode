08ce55fa21b7e83611300c91356facedbd07b39b
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-08-02 18:35:49 -0500

    LeetCode 705

---
+++ AugustChallenge/DesignHashSet.py
@@ -0,0 +1,43 @@
+class MyHashSet(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.array=[[] for _ in range(1000)]
+
+    def add(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        subkey=key%1000
+        if not self.contains(key):
+            self.array[subkey].append(key)
+            
+    def remove(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        subkey=key%1000
+        if self.contains(key):
+            self.array[subkey].remove(key)
+
+    def contains(self, key):
+        """
+        Returns true if this set contains the specified element
+        :type key: int
+        :rtype: bool
+        """
+        subkey=key%1000
+        return  key in self.array[subkey]
+        
+    
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
