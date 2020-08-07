1d2119523f1c167e59361e636dfbd43290788bee
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-08-07 15:53:36 -0500

    LeetCode 204

--- CountPrimes.py
+++ CountPrimes.py
@@ -4,13 +4,16 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        if n <= 2:
+            return 0
         count = 0
-        primes = [False for i in range(n+1)]
+        primes = [True for i in range(n+1)]
+        primes[0],primes[1]=False, False
         for i in range(2,n):
-            if primes[i] == False:
+            if primes[i] == True:
                 count+=1
                 j = 2
                 while j*i<n:
-                    primes[j*i] = True
+                    primes[j*i] = False
                     j+=1
         return count
