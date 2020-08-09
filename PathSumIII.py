0349550e216e98ee6771ad1286ac297c174adcdc
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-08-08 23:15:14 -0500

    LeetCode 437

---
+++ PathSumIII.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: int
+        """
+        def dfs(root, prevSum, sum):
+            if not root:
+                return 0
+            count = 0
+            currSum = prevSum + root.val
+            if currSum - sum in rec:
+                count += rec[currSum - sum]
+            if currSum in rec:
+                rec[currSum] += 1
+            else:
+                rec[currSum] = 1
+            count += dfs(root.left, currSum, sum)
+            count += dfs(root.right, currSum, sum)
+            rec[currSum] -= 1
+            return count
+            
+        rec = {0:1}
+        return dfs(root, 0, sum)
