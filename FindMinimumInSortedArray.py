fba8451ad80c9e80eb0e5be8e9aba8d31d7004d5
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-08-10 11:33:53 -0500

    LeetCode 153

--- FindMinimumInSortedArray.py
+++ FindMinimumInSortedArray.py
@@ -13,5 +13,26 @@ class Solution(object):
                 j=mid
             else:
                 i=mid+1
-        return nums[i]         
+        return nums[i]
+    
+    
+############To find Maximum#################
+
+        i=0
+        j=len(nums)-1
+        
+        while i<j:
+            mid=i+(j-i) // 2 
+            
+            if (mid < j and nums[mid + 1] < nums[mid]): 
+                return nums[mid] 
+                
+            if (mid > i and nums[mid] < nums[mid - 1]): 
+                return nums[mid - 1]    
+            
+            if nums[i]>nums[mid]:
+                j=mid-1
+            else:
+                i=mid+1
+        return nums[i]  
                 
