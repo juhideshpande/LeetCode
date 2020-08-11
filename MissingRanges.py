227fee164f537ea544f927b230a48a8a17b76ba3
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-08-11 10:00:49 -0500

    LeetCode 163

---
+++ MissingRanges.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def findMissingRanges(self, nums, lower, upper):
+        """
+        :type nums: List[int]
+        :type lower: int
+        :type upper: int
+        :rtype: List[str]
+        """
+        nums = [lower-1] + nums + [upper+1]
+        print(nums)
+        res = []
+        
+        for i in range(len(nums)-1):
+            if nums[i+1] - nums[i] == 2:
+                res.append(str(nums[i]+1))
+            elif nums[i+1] - nums[i] > 2:
+                res.append(str(nums[i]+1)+'->'+str(nums[i+1]-1))
+        
+        return res
