08ae80fcf362ee0fa6eeb703454bec5f37bed77d
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-08-11 09:16:26 -0500

    LeetCode 274

---
+++ H-Index.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        citations=sorted(citations)
+        if citations.count(0)==len(citations):
+            return 0
+        low = 0
+        n=len(citations)
+        high=n-1
+        while(low<=high):
+            mid = low+(high-low)/2
+            
+            if citations[mid]==(n-mid):
+                return citations[mid]
+            elif citations[mid]>(n-mid):
+                high=mid-1
+            else:
+                low=mid+1
+                
+        return n-low        
+        
