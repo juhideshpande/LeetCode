7217057cddd987cdfdb03ad24bfd8a35e953f51d
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-08-12 11:01:09 -0500

    LeetCode 273

---
+++ IntegerToEnglishWords.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def numberToWords(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        
+        to19 = 'One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve ' \
+           'Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen'.split()
+        tens = 'Twenty Thirty Forty Fifty Sixty Seventy Eighty Ninety'.split()
+        thousand = 'Thousand Million Billion'.split()
+        
+        def word(num, idx=0):
+            if num == 0:
+                return []
+            if num < 20:
+                return [to19[num-1]]
+            if num < 100:
+                return [tens[num//10-2]] + word(num%10)
+            if num < 1000:
+                return [to19[num//100-1]] + ['Hundred'] + word(num%100)
+
+            p, r = num//1000, num%1000
+            space = [thousand[idx]] if p % 1000 !=0 else []
+            return  word(p, idx+1) + space + word(r)
+        return ' '.join(word(num)) or 'Zero'
+        
