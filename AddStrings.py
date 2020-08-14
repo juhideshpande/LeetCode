5ed27dd29cc66fc8ef5181b3ef256df3e43fb3bc
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-08-13 20:49:21 -0500

    LeetCode 415

---
+++ AddStrings.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """        
+        n1, n2 = 0, 0
+        for i in range(len(num1)):
+            n1 = n1*10 + ord(num1[i])-48
+        for i in range(len(num2)):
+            n2 = n2*10 + ord(num2[i])-48
+        return str(n1+n2)
