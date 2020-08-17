f2d4d0a40aeccf53855218ddf31b15ab1cea6fc8
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-08-16 22:34:12 -0500

    LeetCode 380

--- InsertDeleteGetRandom.py
+++ InsertDeleteGetRandom.py
@@ -1,55 +1,52 @@
-class RandomizedCollection(object):
+class RandomizedSet(object):
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.collection=[]
+        self.dict = {}
+        self.list = []
         
 
     def insert(self, val):
         """
-        Inserts a value to the collection. Returns true if the collection did not already contain the specified element.
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
         :type val: int
         :rtype: bool
         """
-        self.collection.append(val)
-        #    print("insert",self.collection)
+        if val in self.dict:
+            return False
+        self.dict[val] = len(self.list)
+        self.list.append(val)
         return True
-        
 
     def remove(self, val):
         """
-        Removes a value from the collection. Returns true if the collection contained the specified element.
+        Removes a value from the set. Returns true if the set contained the specified element.
         :type val: int
         :rtype: bool
         """
-        try:
-            if val in self.collection:
-                self.collection.remove(val)
-                #print(self.collection)
-                return True
-            else:
-
-                return False
-        except:
-            pass
+        if val in self.dict:
+            # move the last element to the place idx of the element to delete
+            last_element, idx = self.list[-1], self.dict[val]
+            self.list[idx], self.dict[last_element] = last_element, idx
+            # delete the last element
+            self.list.pop()
+            del self.dict[val]
+            return True
+        return False
         
+
     def getRandom(self):
         """
-        Get a random element from the collection.
+        Get a random element from the set.
         :rtype: int
         """
-        try:
-            if self.collection:
-                return random.choice(self.collection)
-        except:
-            pass
-            
+        return random.choice(self.list)
 
 
-# Your RandomizedCollection object will be instantiated and called as such:
-# obj = RandomizedCollection()
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
 # param_1 = obj.insert(val)
 # param_2 = obj.remove(val)
 # param_3 = obj.getRandom()
