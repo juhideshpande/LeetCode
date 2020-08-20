44cd52f9d7a027079a31b79c43976a332813b9d1
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-08-20 17:12:25 -0500

    LeetCode 236

---
+++ LowestCommonAncestorOfBinaryTree.py
@@ -0,0 +1,60 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+#         if p==root or q==root: #time and space complexity: O(N)
+#             return root        
+#         left=right=None        
+#         if root.left:
+#             left = self.lowestCommonAncestor(root.left, p, q)
+#         if root.right:
+#             right = self.lowestCommonAncestor(root.right, p, q)
+            
+#         if left and right:
+#             return root
+#         else:
+#             return left or right
+
+        # Stack for tree traversal
+        stack = [root]
+
+        # Dictionary for parent pointers
+        parent = {root: None}
+
+        # Iterate until we find both the nodes p and q
+        while p not in parent or q not in parent:
+
+            node = stack.pop()
+
+            # While traversing the tree, keep saving the parent pointers.
+            if node.left:
+                parent[node.left] = node
+                stack.append(node.left)
+            if node.right:
+                parent[node.right] = node
+                stack.append(node.right)
+
+        # Ancestors set() for node p.
+        ancestors = set()
+
+        # Process all ancestors for node p using parent pointers.
+        while p:
+            ancestors.add(p)
+            p = parent[p]
+
+        # The first ancestor of q which appears in
+        # p's ancestor set() is their lowest common ancestor.
+        while q not in ancestors:
+            q = parent[q]
+        return q
