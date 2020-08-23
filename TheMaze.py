001afb5ad63476166a59176a7d80901c41a72864
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-08-23 00:00:08 -0500

    LeetCode 490

--- TheMaze.py
+++ TheMaze.py
@@ -1,21 +1,29 @@
-r = len(maze)
-		c = len(maze[0])
-		path = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-		stack = [(start[0], start[1])]        
-		seen = {(start[0], start[1])}
-		while stack:
-			x, y = stack.pop()
-			for a, b in path:
-				nx = x
-				ny = y
-				while 0 <= nx < r and 0 <= ny < c and maze[nx][ny] == 0:
-					nx += a
-					ny += b
-				nx -= a
-				ny -= b
-				if [nx, ny] == destination:
-					return True
-				else:
-					if (nx, ny) not in seen:
-						stack.append((nx, ny))
-						seen.add((nx, ny))
+class Solution(object):
+    def hasPath(self, maze, start, destination):
+        """
+        :type maze: List[List[int]]
+        :type start: List[int]
+        :type destination: List[int]
+        :rtype: bool
+        """
+        r = len(maze)
+        c = len(maze[0])
+        path = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        stack = [(start[0], start[1])]        
+        seen = {(start[0], start[1])}
+        while stack:
+          x, y = stack.pop()
+          for a, b in path:
+            nx = x
+            ny = y
+            while 0 <= nx < r and 0 <= ny < c and maze[nx][ny] == 0:
+              nx += a
+              ny += b
+            nx -= a
+            ny -= b
+            if [nx, ny] == destination:
+              return True
+            else:
+              if (nx, ny) not in seen:
+                stack.append((nx, ny))
+                seen.add((nx, ny))
