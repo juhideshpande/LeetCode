19f78b21b9cedd05d50e5cb158bf80fb09da740e
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-09-16 20:50:30 -0500

    LeetCode 421

---
+++ MaximumXOROfTwoNumbersInAnArray.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findMaximumXOR(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ans, mask = 0, 0
+        for i in range(31, -1, -1):
+            mask |= 1<<i
+            found = set([num & mask for num in nums])
+                
+            start = ans | 1<<i
+            for pref in found:
+                if start^pref in found:
+                    ans = start
+                    break
+         
+        return ans
