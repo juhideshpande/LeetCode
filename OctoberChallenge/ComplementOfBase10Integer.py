07fa76f8ce2a1c73dfb5226cbf328fd0fda62931
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-10-05 10:31:50 -0500

    LeetCode 1009

---
+++ OctoberChallenge/ComplementOfBase10Integer.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def bitwiseComplement(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        X = 1
+        while N > X: 
+            X = X * 2 + 1
+        return X - N
