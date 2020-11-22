79a8e7befeb8d0a9b4e81971ecde3888e130d3ae
juhideshpande <46695727+juhideshpande@users.noreply.github.com>
2020-11-22 10:57:27 -0600

    LeetCode 938

---
+++ NovemberChallenge/RangeSumOfBST.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    
+    def rangeSumBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: int
+        """
+        count=0
+        ans = []
+        stack = []
+    
+        while stack or root:
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                tmpNode = stack.pop()
+                ans.append(tmpNode.val)
+                root = tmpNode.right
+        
+        start=ans.index(L)  
+        end=ans.index(R)
+        for i in range(len(ans)):
+            if (i>=start and i<=end):
+                count+=ans[i]
+                
+        return count        
